4ad8c558c3d2c6482ed2d53975ca79f94464914a
adrianre12 <adrianrevill@hotmail.com>
2026-01-10 17:54:38 +0000

    buttons working

---
+++ NotificationBlock/NotificationComponent.cs
@@ -0,0 +1,60 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.ModAPI;
+using VRage.Game.Components;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+
+namespace ZoneControl.NotificationBlock
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_TextPanel), false, new[] { "NotificationBlock" })]
+    internal class NotificationComponent : MyGameLogicComponent
+    {
+        private IMyFunctionalBlock block;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            if (!MyAPIGateway.Session.IsServer)
+                return;
+
+            block = Entity as IMyFunctionalBlock;
+
+            NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            base.UpdateOnceBeforeFrame();
+            block.Physics = null; //disable the mass
+
+            NeedsUpdate = MyEntityUpdateEnum.EACH_100TH_FRAME;
+        }
+        public override void UpdateAfterSimulation100()
+        {
+            Log.Msg($"Tick {block.CubeGrid.DisplayName}");
+        }
+
+        public override void OnAddedToScene()
+        {
+            base.OnAddedToScene();
+            Log.Msg($"OnAddedToScene {block.CubeGrid.DisplayName}");
+        }
+
+        public override void OnRemovedFromScene()
+        {
+            Log.Msg($"OnRemovedFromScene {block.CubeGrid.DisplayName}");
+
+        }
+
+        public override void MarkForClose()
+        {
+            base.MarkForClose();
+            Log.Msg($"MarkForClose {block.CubeGrid.DisplayName}");
+
+        }
+
+        internal void ButtonPressed(IMyEntity user, int button)
+        {
+            Log.Msg($"Button pressed: {button}");
+        }
+    }
+}
---
+++ NotificationBlock/NotificationUseObject.cs
@@ -0,0 +1,70 @@
+﻿using Sandbox.ModAPI;
+using VRage.Game.Entity.UseObject;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.Utils;
+
+namespace ZoneControl.NotificationBlock
+{
+    [MyUseObject("zonenote")]
+    public class Button1 : MyUseObjectBase
+    {
+        private IMyTextPanel block;
+        private NotificationComponent component;
+
+        public override UseActionEnum SupportedActions => UseActionEnum.Manipulate;
+        // | UseActionEnum.Close
+        // | UseActionEnum.BuildPlanner
+        // | UseActionEnum.OpenInventory
+        // | UseActionEnum.OpenTerminal
+        // | UseActionEnum.PickUp
+        // | UseActionEnum.UseFinished; // gets called when releasing manipulate
+
+        // What action gets sent to Use() when interacted with PrimaryAttack or Use binds.
+        public override UseActionEnum PrimaryAction => UseActionEnum.Manipulate;
+
+        // What action gets sent to Use() when interacted with SecondaryAttack or Inventory/Terminal binds.
+        public override UseActionEnum SecondaryAction => UseActionEnum.OpenTerminal;
+
+
+        public Button1(IMyEntity owner, string dummyName, IMyModelDummy dummyData, uint shapeKey) : base(owner, dummyData)
+        {
+            block = owner as IMyTextPanel;
+            component = block.GameLogic.GetAs<NotificationComponent>();
+        }
+
+        public override MyActionDescription GetActionInfo(UseActionEnum actionEnum)
+        {
+            switch (actionEnum)
+            {
+                default:
+                    return default(MyActionDescription);
+
+                case UseActionEnum.Manipulate:
+                    return new MyActionDescription()
+                    {
+                        Text = MyStringId.GetOrCompute("Press to select"),
+                        IsTextControlHint = true,
+                    };
+            }
+        }
+
+        public override void Use(UseActionEnum actionEnum, IMyEntity user)
+        {
+            switch (actionEnum)
+            {
+                case UseActionEnum.Manipulate:
+                    {
+                        var button = int.Parse(this.Dummy.Name.Substring(this.Dummy.Name.LastIndexOf('_') + 1));
+                        if (component != null)
+                        {
+                            component.ButtonPressed(user, button);
+                        }
+
+                        break;
+                    }
+            }
+        }
+
+    }
+}
